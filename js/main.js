cac4af9d1ed08b5fb4d327c13e2e3e1c41942dfd
craigmd <craigmichaeldickson@gmail.com>
2016-08-22 00:50:47 -0400

    basic layout and nav functionality

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const menu = document.getElementById('menu');
+
+let showNav = () => {
+  const nav = document.getElementById('popout-nav');
+  nav.classList.add('is-active');
+}
+
+menu.addEventListener('click', showNav, false);
+
+const exitButton = document.getElementById('exit');
+
+let hideNav = () => {
+  const nav = document.getElementById('popout-nav');
+  nav.classList.remove('is-active');
+}
+
+exitButton.addEventListener('click', hideNav, false);
