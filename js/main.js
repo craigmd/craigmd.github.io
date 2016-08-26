7679b832a3d6a10744b68774a09c5a6fdc687ac0
craigmd <craigmichaeldickson@gmail.com>
2016-08-26 00:30:40 -0400

    Redoing the whole thing! Made new background for header

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-//Popout Menu
-const menu = document.getElementById('menu');
-
-let showNav = () => {
-  const nav = document.getElementById('popout-nav');
-  nav.classList.add('is-active');
-}
-
-menu.addEventListener('click', showNav, false);
-
-const exitButton = document.getElementById('exit');
-
-let hideNav = () => {
-  const nav = document.getElementById('popout-nav');
-  nav.classList.remove('is-active');
-}
-
-exitButton.addEventListener('click', hideNav, false);
+// //Popout Menu
+// const menu = document.getElementById('menu');
+//
+// let showNav = () => {
+//   const nav = document.getElementById('popout-nav');
+//   nav.classList.add('is-active');
+// }
+//
+// menu.addEventListener('click', showNav, false);
+//
+// const exitButton = document.getElementById('exit');
+//
+// let hideNav = () => {
+//   const nav = document.getElementById('popout-nav');
+//   nav.classList.remove('is-active');
+// }
+//
+// exitButton.addEventListener('click', hideNav, false);
