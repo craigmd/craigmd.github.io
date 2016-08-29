a8b8deaea396e8816b549aef701939d4e0f2f3f5
craigmd <craigmichaeldickson@gmail.com>
2016-08-28 22:32:20 -0400

    Updated Nav, changed fonts

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-// //Popout Menu
-// const menu = document.getElementById('menu');
-//
-// let showNav = () => {
-//   const nav = document.getElementById('popout-nav');
-//   nav.classList.add('is-active');
-// }
-//
-// menu.addEventListener('click', showNav, false);
-//
-// const exitButton = document.getElementById('exit');
-//
-// let hideNav = () => {
-//   const nav = document.getElementById('popout-nav');
-//   nav.classList.remove('is-active');
-// }
-//
-// exitButton.addEventListener('click', hideNav, false);
+//Popout Menu
+const menu = document.getElementById('menu');
+const exitButton = document.getElementById('exit');
+
+let showNav = () => {
+  const nav = document.getElementById('popout-nav');
+  nav.classList.add('is-active');
+}
+
+let hideNav = () => {
+  const nav = document.getElementById('popout-nav');
+  nav.classList.remove('is-active');
+}
+
+menu.addEventListener('click', showNav, false);
+exitButton.addEventListener('click', hideNav, false);
