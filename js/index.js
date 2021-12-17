66682a46a70433110fe5fc3501a24c5102a9da31
craigmd <craigmichaeldickson@gmail.com>
2021-12-17 15:12:24 -0500

    Laura's christmas update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 
+const sections = ['intro', 'tease', 'cabin', 'sleigh', 'reveal'];
+const nextButton = document.getElementById('next-section-button');
+
+let currentSectionIndex = 0;
+
+nextButton.addEventListener('click', () => {
+  const nextIndex = (currentSectionIndex + 1) % sections.length;
+  const nextSection = document.getElementById(sections[nextIndex]);
+  const text = document.getElementById('next-section-text');
+  const chevron = document.getElementById('next-section-img');
+
+  if (nextIndex === sections.length - 1) {
+    chevron.src = './images/chevron-up.svg';
+    text.innerText = 'Back to top';
+  } else {
+    chevron.src = './images/chevron-down.svg';
+    text.innerText = 'Let\'s go';
+  }
+  
+  if (nextSection) {
+    currentSectionIndex = nextIndex;
+    nextSection.scrollIntoView({ behavior: 'smooth' });
+  }
+});
